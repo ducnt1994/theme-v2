e885ef5ec49c621feed9faea63c2fd7939bec725
ducbeo <ducntse03282@gmail.com>
2025-05-14 22:46:33 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import BannerExtra1 from "@/components/page-builders/banners/extra/BannerExtra1";
 
 export default function Home() {
 
   return (
     <div className={styles.page}>
-      <BannerExtra1/>
       <main className={styles.main}>
         <Image
           className={styles.logo}
